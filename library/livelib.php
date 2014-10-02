0133d96464bb1cd8a3c296e495876cba059f293d
apolunin <apolunin@apolunin.(none)>
2014-10-02 12:35:36 +0400

    removed

--- library/livelib.php
+++
@@ -1,6 +0,0 @@
-<?php
-//echo gzdecode(file_get_contents('C:\\xampp\\htdocs\\cache\\livelib_cache\\2\1\\521.qz'));
-while(true){
-file_get_contents('http://livelib.ru/');
-}
-?>
--- livelib_crawler/test.php
+++
@@ -1,6 +0,0 @@
-<?
-
-$s = '</b></td><td><a rel="zauthor" href="bookbyauthor.php?author=6310" title="Дюма Александр"><strong>Дюма Александр</strong>';
-
-echo strip_tags($s);
-?>
